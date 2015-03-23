805aaccc0331611f4507c1133cf8591401679a5a
jenhail <hello@jenniferhail.com>
2015-03-22 23:36:57 -0400

    add acf to home page

--- front-page.php
+++ front-page.php
@@ -9,13 +9,24 @@ get_header(); ?>
 
 <section id="primary" role="main" class="row">
 
+	<div class="featured-image">
+		<?php 
+
+		$image = get_field('featured_image');
+
+		if( !empty($image) ): ?>
+
+			<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+		<?php endif; ?>
+	</div>
+
+	<h2 class="caption-left"><?php the_field('left_caption'); ?></h2>
+	<h2 class="caption-right"><?php the_field('right_caption'); ?></h2>
+
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header><!-- .entry-header -->
-	
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>	
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'pine' ) . '</span>', 'after' => '</div>' ) ); ?>
