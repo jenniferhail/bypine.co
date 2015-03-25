4200bd810ec8ca2770bc4a14e6284f49669879bb
jenhail <hello@jenniferhail.com>
2015-03-24 22:12:49 -0400

    use my logo on the login page

--- functions.php
+++ functions.php
@@ -47,6 +47,29 @@ function pine_setup() {
 endif;
 add_action( 'after_setup_theme', 'pine_setup' );
 
+/* THEME LOGIN LOGO
+ ========================== */
+function my_login_logo() { ?>
+    <style type="text/css">
+        .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>assets/images/logo.png);
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+// Change the link values so the logo links to my WordPress site
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Pine';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
 
 /* SIDEBARS & WIDGET AREAS
  ========================== */
