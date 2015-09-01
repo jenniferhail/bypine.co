54d95a25b5a551a4fce153d1576118e053f14710
jenhail <hello@jenniferhail.com>
2015-08-31 22:31:01 -0400

    remove pop up

--- front-page.php
+++ front-page.php
@@ -81,33 +81,6 @@ get_header(); ?>
 
 </section><!-- #blog-feed -->
 
-<p>Please <a class="show-popup" href="#" data-showpopup="1" >click here</a> to see the popup.</p>
-<div class="overlay-bg">
-</div>
-<!-- Begin MailChimp Signup Form -->
-	<div class="overlay-content popup1" id="mc_embed_signup">
-	<form action="//bypine.us10.list-manage.com/subscribe/post?u=90363b999232548e9532004b0&amp;id=874c472f49" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	    <div id="mc_embed_signup_scroll">
-		<button class="close-btn">X</button>
-	<div class="mc-field-group">
-		<label for="mce-EMAIL">Email Address </label>
-		<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-	</div>
-	<div class="mc-field-group">
-		<label for="mce-FNAME">First Name </label>
-		<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-	</div>
-		<div id="mce-responses" class="clear">
-			<div class="response" id="mce-error-response" style="display:none"></div>
-			<div class="response" id="mce-success-response" style="display:none"></div>
-		</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	    <div style="position: absolute; left: -5000px;"><input type="text" name="b_90363b999232548e9532004b0_874c472f49" tabindex="-1" value=""></div>
-	    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-	    </div>
-	</form>
-	</div>
-<!--End mc_embed_signup-->
-
 <script>
 <!--
 home_navbar();
