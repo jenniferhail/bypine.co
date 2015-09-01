753fc4fe0850adbc91cfc4b5af665e469946cc70
jenhail <hello@jenniferhail.com>
2015-08-31 22:52:13 -0400

    attempt to match pages to the front page

--- content-page.php
+++ content-page.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * The template used for displaying page content in page.php
+ * Page Template
  *
  * @package Pine
  */
-?>
+
+get_header(); ?>
 
 <section id="primary" role="main" class="row">
 
@@ -46,15 +47,45 @@
 
 	</div>
 
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article class="two-column">
+			<?php the_content(); ?>
+		</article><!-- .entry-content -->
+	<?php endwhile; // end of the loop. ?>
+
+	<article class="one-column">
+		<h3 class="question"><?php the_field('one_column_title'); ?></h3>
+		<div class="answer"><?php the_field('one_column_content'); ?></div>
+	</article>
+
 </section><!-- #primary -->
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
+<section id="blog-feed" class="row">
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'pine' ) . '</span>', 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-</article><!-- #post-<?php the_ID(); ?> -->
+	<?php
+	// The Query
+	$blog_feed = new WP_Query( 'post_count=4' ); ?>
+
+	<?php if ( $blog_feed->have_posts() ) : ?>
+
+		<?php while ( $blog_feed->have_posts() ) : $blog_feed->the_post(); ?>
+
+				<?php get_template_part( 'content', 'summary' ); ?>
+
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+
+	<?php else : ?>
+		<!-- Do Nothing -->
+	<?php endif; ?>
+
+</section><!-- #blog-feed -->
+
+<script>
+<!--
+home_navbar();
+pine_letter();
+//-->
+</script>
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -9,15 +9,11 @@
 
 get_header(); ?>
 
-<section id="primary" role="main" class="col pull-left span_8">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'page' ); ?>
+	<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php comments_template( '', true ); ?>
+<?php endwhile; // end of the loop. ?>
 
-		<?php endwhile; // end of the loop. ?>
-
-</section><!-- #primary -->
 <?php get_footer(); ?>
