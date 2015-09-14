8289eaefc64ef656dc56992f1600a2234df188eb
jenhail <hello@jenniferhail.com>
2015-09-13 23:40:58 -0400

    remove positioning from header

--- header.php
+++ header.php
@@ -62,15 +62,16 @@
 
         <header>
 
-            <div class="home-logo">
-                <a href="<?php echo esc_url( home_url() ); ?>/" class="logo">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
-                </a>
+            <div class="left">
+                <div class="home-logo">
+                    <a href="<?php echo esc_url( home_url() ); ?>/" class="logo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
+                    </a>
+                </div>
+                <nav class="access" role="navigation">
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+                </nav><!-- #access -->             
             </div>
-
-            <nav class="access" role="navigation">
-                <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-            </nav><!-- #access --> 
 
             <div class="featured-image">
             
