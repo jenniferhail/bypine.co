9abe080e564cb5fb9f786fe5ee35d8be5ed731a7
jenhail <hello@jenniferhail.com>
2015-09-14 00:58:20 -0400

    move the caption right for mobile

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <title><?php wp_title( '|', true, 'left' ); ?></title>
     
     <!-- favicon & links -->
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon">
