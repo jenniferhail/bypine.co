50fece8ee2897fdb8317351e049ba9e5f8a0231b
jenhail <hello@jenniferhail.com>
2015-09-13 23:35:46 -0400

    adjust header layout

--- content-page.php
+++ content-page.php
@@ -9,45 +9,6 @@ get_header(); ?>
 
 <section id="primary" role="main" class="row">
 
-	<div class="home-logo">
-		<a href="<?php echo esc_url( home_url() ); ?>/" class="logo">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
-		</a>
-	</div>
-
-	<div class="featured-image">
-
-        <nav class="access" role="navigation">
-            <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-        </nav><!-- #access --> 
-	
-		<?php 
-
-		$image = get_field('featured_image');
-
-		if( !empty($image) ): ?>
-
-			<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-
-		<?php endif; ?>
-
-		<div class="caption-left">
-			<?php 
-
-			$image = get_field('left_caption');
-
-			if( !empty($image) ): ?>
-
-				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-
-			<?php endif; ?>
-		</div>
-		<?php if( the_field('right_caption') ): ?>
-			<h2 class="caption-right"><?php the_field('right_caption'); ?></h2>
-		<?php endif; ?>
-
-	</div>
-
 	<?php if( the_content() ): ?>
 		<article class="two-column">
 			<?php the_content(); ?>
--- header.php
+++ header.php
@@ -59,4 +59,48 @@
 			</a>
 		</header>
         -->
+
+        <header>
+
+            <div class="home-logo">
+                <a href="<?php echo esc_url( home_url() ); ?>/" class="logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
+                </a>
+            </div>
+
+            <nav class="access" role="navigation">
+                <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+            </nav><!-- #access --> 
+
+            <div class="featured-image">
+            
+                <?php 
+
+                $image = get_field('featured_image');
+
+                if( !empty($image) ): ?>
+
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+                <?php endif; ?>
+
+                <div class="caption-left">
+                    <?php 
+
+                    $image = get_field('left_caption');
+
+                    if( !empty($image) ): ?>
+
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+                    <?php endif; ?>
+                </div>
+                <?php if( the_field('right_caption') ): ?>
+                    <h2 class="caption-right"><?php the_field('right_caption'); ?></h2>
+                <?php endif; ?>
+
+            </div>            
+
+        </header>
+
 		<div id="main" class="row">
